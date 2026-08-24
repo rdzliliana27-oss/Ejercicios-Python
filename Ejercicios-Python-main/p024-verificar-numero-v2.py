6d677690b7ec6650e7a2ff53bd239bab3cc296e1
rdzliliana27-oss <rdzliliana27@gmail.com>
2026-08-24 12:26:45 -0600

    otro

--- Ejercicios-Python-main/p024-verificar-numero-v2.py
+++ Ejercicios-Python-main/p024-verificar-numero-v2.py
@@ -4,12 +4,12 @@ def main():
     numero = int(input("Ingrese un numero entero: "))
 
     if numero > 0:
-        print("El numero es POSITIVO.")
+        print("El numero es POSITIVO 👍.")
     else:
         if numero < 0:
-            print("El numero es NEGATIVO.")
+            print("El numero es NEGATIVO 👎.")
         else:
-            print("El numero es CERO.")
+            print("El numero es CERO 😐")
 
     print("Aqui terminamos de tomar decisiones.")
 
